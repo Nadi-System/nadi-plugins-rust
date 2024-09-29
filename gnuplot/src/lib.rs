cc626e374c7da5bcbbdc2d9bc2b7fd816e13a93d
Gaurav Atreya <allmanpride@gmail.com>
2024-09-29 01:12:52 -0400

    Add gnuplot plugin with custom data type

---
+++ gnuplot/src/lib.rs
@@ -0,0 +1,106 @@
+use nadi_core::nadi_plugin::nadi_plugin;
+
+#[nadi_plugin]
+mod gnuplot {
+    use abi_stable::std_types::Tuple2;
+    use nadi_core::attrs::FromAttribute;
+    use nadi_core::nadi_plugin::network_func;
+    use nadi_core::{AttrMap, Attribute, Network};
+    use std::{fs::File, io::Write, path::PathBuf};
+    use string_template_plus::Template;
+
+    #[derive(Debug, Default)]
+    struct GnuplotConfig {
+        outfile: Option<PathBuf>,
+        terminal: Option<String>,
+        csv: bool,
+        preamble: String,
+        // TODO add more config options for gnuplot
+    }
+
+    // If you need custom values, you can implement FromAttribute, or
+    // FromAttributeRelaxed for your type; this will allow any custom
+    // type to be used in your function.
+    impl FromAttribute for GnuplotConfig {
+        fn from_attr(value: &Attribute) -> Option<Self> {
+            Self::try_from_attr(value).ok()
+        }
+
+        fn try_from_attr(value: &Attribute) -> Result<Self, String> {
+            let tab = AttrMap::try_from_attr(value)?;
+            let mut config = Self::default();
+            for Tuple2(k, v) in &tab {
+                match k.as_str() {
+                    "outfile" => {
+                        config.outfile = Some(PathBuf::try_from_attr(v)?);
+                    }
+                    "terminal" => {
+                        config.terminal = Some(String::try_from_attr(v)?);
+                    }
+                    "csv" => {
+                        config.csv = bool::try_from_attr(v)?;
+                    }
+                    "preamble" => {
+                        config.preamble = String::try_from_attr(v)?;
+                    }
+                    v => return Err(format!("unknown key {v:?} in gnuplot config")),
+                }
+            }
+            Ok(config)
+        }
+    }
+
+    /// Generate a gnuplot file that plots the timeseries data in the network
+    #[network_func(timefmt = "%Y-%m-%d", skip_missing = false, config = GnuplotConfig::default())]
+    fn plot_timeseries(
+        net: &mut Network,
+        csvfile: Template,
+        datecol: String,
+        datacol: String,
+        outfile: PathBuf,
+        timefmt: String,
+        config: GnuplotConfig,
+        skip_missing: bool,
+    ) -> anyhow::Result<()> {
+        let mut plot_lines = Vec::with_capacity(net.nodes_count());
+        for node in net.nodes() {
+            let node = node.lock();
+            let path = node.render(&csvfile)?;
+            if skip_missing && !PathBuf::from(&path).exists() {
+                continue;
+            }
+            plot_lines.push(format!(
+                "plot {path:?} using {datecol:?}:{datacol:?} with lines"
+            ));
+        }
+        let nodes_count = plot_lines.len();
+
+        let mut file = File::create(outfile)?;
+        if config.csv {
+            writeln!(file, "set datafile separator \",\"")?;
+        }
+        write!(
+            file,
+            "
+set xdata time
+set timefmt {timefmt:?}
+unset key
+set xtics format \"%Y\" rotate
+set mxtics 6
+set multiplot layout {nodes_count},1
+"
+        )?;
+        if let Some(term) = config.terminal {
+            writeln!(file, "set terminal {term}")?;
+        }
+        if let Some(out) = config.outfile {
+            writeln!(file, "set output {out:?}")?;
+        }
+        writeln!(file, "{}", config.preamble)?;
+        for line in plot_lines {
+            writeln!(file, "{line}")?;
+        }
+
+        Ok(())
+    }
+}
